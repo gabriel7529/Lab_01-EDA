6b231fd86f46b72ce52dbecce623b4d8ed867385
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-05-08 16:35:11 -0500

    Se agrega un println con el nombre.

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,6 +2,9 @@ public class HolaMundo{
     public static void main(String args[]) {
         //Se imprime un Hola Mundo
         System.out.println ("¡Hola mundo!");
-	System.out.println ("Bienvenido al curso de laboratorio EDA"); 
+	//Se imprime una bienvenida
+	System.out.println ("Bienvenido al curso de laboratorio EDA");
+       //Se imprime el nombre
+       System.out.println ("Mi nombre es:  Gabriel Steven Machicao Quispe");	
     }
 }
