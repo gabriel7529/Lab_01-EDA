d55e2e8254208a9ce5fc8e55b49d8e3c153644cf
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-05-08 16:33:20 -0500

    Se agrego como un mensaje de Bienvenida.

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,5 +2,6 @@ public class HolaMundo{
     public static void main(String args[]) {
         //Se imprime un Hola Mundo
         System.out.println ("¡Hola mundo!");
+	System.out.println ("Bienvenido al curso de laboratorio EDA"); 
     }
 }
